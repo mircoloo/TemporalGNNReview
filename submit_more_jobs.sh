a751a7259f07c48b9226a7dd0bd133d3731fd7d3
Mirco <bismirco@gmail.com>
2025-10-22 13:18:16 +0000

    Remove submit_more_jobs.sh script as it is no longer needed

--- submit_more_jobs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-pwd
-bash ./submit_job.sh sse dgdnn zscore False
-bash ./submit_job.sh sse dgdnn none False
-bash ./submit_job.sh sse dgdnn log1p False
-bash ./submit_job.sh sse dgdnn minmax False
